5c65160ca04abdbc4442bbd66cdc8dfee3becafe
รามา มนูสัมฤทธิ์ <rama81te@gmail.com>
2026-07-16 23:59:06 +0700

    Fix Turso authentication passing

--- web/src/lib/prisma.ts
+++ web/src/lib/prisma.ts
@@ -2,15 +2,19 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-import { PrismaLibSql } from '@prisma/adapter-libsql'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
 const dbUrl = process.env.DATABASE_URL 
   ? process.env.DATABASE_URL.replace('file:../../', 'file:../') 
   : 'file:./dev.db'
 
-const adapter = new PrismaLibSql({
+const libsql = createClient({
   url: dbUrl,
+  authToken: process.env.TURSO_AUTH_TOKEN,
 })
+
+const adapter = new PrismaLibSQL(libsql)
 
 export const prisma =
   globalForPrisma.prisma ||
